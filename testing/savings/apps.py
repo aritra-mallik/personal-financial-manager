742ee69800c4acb18a2d56f8b294d756b6d5e49b
Aritra Mallik <aritramallik2002@gmail.com>
2025-10-20 23:45:10 +0530

    savings update

---
+++ testing/savings/apps.py
@@ -0,0 +1,9 @@
+# savings/apps.py
+from django.apps import AppConfig
+
+class SavingsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'savings'
+
+    def ready(self):
+        import savings.signals  # noqa
