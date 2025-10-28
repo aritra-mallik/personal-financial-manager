2607d6498028a15fade4dc642c2069338af20beb
Aritra Mallik <aritramallik2002@gmail.com>
2025-10-28 19:02:15 +0530

    Delete testing/core directory

--- testing/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('privacy/', views.privacy, name='privacy'),
-    path('terms/', views.terms, name='terms'),
-    path('settings/', views.settings_view, name='settings_view'),
-    path('update_preferences/', views.update_preferences, name='update_preferences'),
-]
